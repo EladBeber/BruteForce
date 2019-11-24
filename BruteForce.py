47fcfa4ebb9568b94cacb8c9e4c9a5ee711bd749
EdbR <57364083+EladBeber@users.noreply.github.com>
2019-11-24 14:09:04 +0200

    Create BruteForce.py

---
+++ BruteForce.py
@@ -0,0 +1,28 @@
+# Bruteforce tool by EdbR
+import itertools
+
+# 1.Numbers - 0123456789
+def Numbers(Numbers):
+    for i in range(48, 58):
+        Numbers.append(chr(i))
+
+# 2.UpperCase - ABCDEFGHIJKLMNOPQRSTUVWXYZ
+def UpperCase(UpperCase):
+    for i in range(65, 91):
+        UpperCase.append(chr(i))
+
+# 3.lowerCase - abcdefghijklmnopqrstuvwxyz
+def lowerCase(lowerCase):
+    for i in range(97, 123):
+        lowerCase.append(chr(i))
+
+# 4.Bruteforce - All the options - (len(list) ^ LenOfFlag)
+def Bruteforce(list,LenOfFlag):
+    iterator=itertools.product(list,repeat=LenOfFlag)
+    for i in iterator:
+        flag="".join(i)
+        print(flag)
+
+
+
+
