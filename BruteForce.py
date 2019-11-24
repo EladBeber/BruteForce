fb72cbc11318d799cd1a19fb4cc5cb40f9b2fd41
EdbR <57364083+EladBeber@users.noreply.github.com>
2019-11-24 15:14:28 +0200

    Update BruteForce.py

--- BruteForce.py
+++ BruteForce.py
@@ -18,10 +18,18 @@ def lowerCase(lowerCase):
 
 # 4.Bruteforce - All the options - (len(list) ^ LenOfFlag)
 def Bruteforce(list,LenOfFlag):
+    f=open("bdika2.txt","w")
     iterator=itertools.product(list,repeat=LenOfFlag)
     for i in iterator:
         flag="".join(i)
+        f.write(flag)
         print(flag)
+    f.close()
+
+list=[]
+Numbers(list)
+lowerCase(list)
+Bruteforce(list,2)
 
 
 
